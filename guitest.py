191f23b757176f60c4460a68f98d89b2a0f7229e
rihi52 <rileyhire7@gmail.com>
2024-09-24 20:50:02 -0400

    more changes for qt practice

--- guitest.py
+++ guitest.py
@@ -1,10 +1,21 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
+from PyQt6.QtCore import QSize, Qt
 
 import sys
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        button = QPushButton("Press Me!")
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+        
 app = QApplication(sys.argv)
 
-window = QWidget()
+window = MainWindow()
 window.show()
 
 app.exec()
